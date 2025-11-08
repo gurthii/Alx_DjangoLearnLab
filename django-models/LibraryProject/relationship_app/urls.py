7370cfaab0d514778d8b2d561266b96b5b5755d2
Joseph G <josef.emu@gmail.com>
2025-11-09 01:01:51 +0300

    updated for grading #11

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('member-view/', member_view, name='member_view'),
 
     # Secured book management views
-    path('book/add/', views.add_book, name='add_book'),
-    path('book/<int:pk>/edit/', views.edit_book, name='edit_book'),
+    path('book/add/', views.add_book, name='add_book/'),
+    path('book/<int:pk>/edit/', views.edit_book, name='edit_book/'),
     path('book/<int:pk>/delete/', delete_book, name='delete_book'),
 ]
