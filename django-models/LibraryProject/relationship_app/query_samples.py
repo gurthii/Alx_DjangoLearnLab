83e7fe433404cc168db862e4d5f051560c7603e5
Joseph G <josef.emu@gmail.com>
2025-11-09 00:28:55 +0300

    updated for grading

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -13,7 +13,8 @@ def get_books_in_library(library_name):
 # Query 3: Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    librarian = Librarian.objects.get(library=library)
+    return librarian
 
 
 # Example usage (only works in Django shell or after setup)
