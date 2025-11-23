87dbc08930fe9f3c3fe0f89869c24c52be7e3b9d
Joseph G <josef.emu@gmail.com>
2025-11-23 18:09:19 +0300

    autochecker correction #5

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views as auth_views
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('api-token-auth/', auth_views.obtain_auth_token) 
+
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,4 @@
-from  rest_framework import generics, viewsets
+from  rest_framework import generics, viewsets, permissions
 from .models import Book
 from .serializers import BookSerializer
 
@@ -9,3 +9,14 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    # This method dynamically sets permissions based on the action being performed
+    def get_permissions(self):
+        # Allow any authenticated user to list or retrieve books (GET requests)
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [permissions.IsAuthenticated]
+        # Restrict create, update, and destroy actions (POST, PUT, DELETE) to Admin users only
+        else:
+            permission_classes = [permissions.IsAdminUser]
+            
+        return [permission() for permission in permission_classes]
